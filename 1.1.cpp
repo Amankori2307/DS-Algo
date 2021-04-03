f0a4c1de81289d37ee9ab5ecd6cff98df7933c33
Amankori2307 <amankori2307@gmail.com>
2021-04-03 11:55:50 +0530

    Initial Commit

---
+++ 1.1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a; //declaration
+    a = 5; //initialisation
+    cout<<"Size of int is: "<<sizeof(a)<<endl;
+
+    float f;
+    f = 3.14;
+    cout<<"Size of float is: "<<sizeof(f)<<endl;
+
+    char c;
+    c = 'c';
+    cout<<"Size of char is: "<<sizeof(c)<<endl;
+
+    return 0;
+}
