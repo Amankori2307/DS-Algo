6fccd12d2c70dbac4bd0639052babce649f4f589
amankori2307 <amankori2307@gmail.com>
2022-03-04 05:54:05 +0530

    Completed lecture 32.3

---
+++ 32.3.1.cpp
@@ -0,0 +1,30 @@
+/*
+    PROBLEM: Count frequency of elements
+
+*/ 
+
+
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    map<int, int> freq;
+    for(int i=0; i<n; i++){
+        int el = arr[i];
+        freq[el]++;
+    }
+
+    map<int, int> :: iterator it;
+    for(it = freq.begin(); it!=freq.end(); it++){
+        cout << it->first << ": "<< it->second << endl;
+    }
+    return 0;
+}
