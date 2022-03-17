df09286f4fccd36d0d9d2f5168eacc9185fa5a94
amankori2307 <amankori2307@gmail.com>
2022-03-17 05:56:07 +0530

    Cycle Detection In Graph

--- 34.7.1.cpp
+++ 34.7.1.cpp
@@ -1,19 +1,53 @@
 /*
     Cycle Detection
         - In Undirected Graph
-        - In Directed Graph
-*/
 
-#include <iostream>
-#include <vector>
+    Cycle detection in a undirected graph
+
+    Keep on traversing the graph
+    If you find an edge pointing to an already visited node(except the parent node), a cycle is found.
+
+
+
+
+*/
+#include<iostream>
+#include<vector>
 using namespace std;
 
+bool isCycle(int src, vector<vector<int>> &adj, vector<bool> visited, int parent){
+    visited[src] = true;
+    for(auto i : adj[src]){
+        if(i != parent){
+            if(visited[i]){
+                return true;
+            } else if(!visited[i] && isCycle(i, adj, visited, src)){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 int main() {
-    int n, e; cin >> n >> e;
+    int n, m; cin >> n >> m;
     vector<vector<int>> adj(n);
 
-    for(int i=0; i<e; i++){
+    for(int i=0; i<m; i++){
         int x, y; cin >> x >> y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
     }
-    return 0;
+
+
+    bool cycle = false;
+    vector<bool> isVisited(m, false);
+
+    for(int i=0; i<n; i++){
+        if(!isVisited[i] && isCycle(i, adj, isVisited, -1)){
+            cycle = true;
+        }
+    }
+
+    cout << cycle << endl;
 }
